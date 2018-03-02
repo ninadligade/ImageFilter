a4135168b69be9f2cfccdde36d09898e1ac28015
Ninad Ligade <ninadligade@gmail.com>
2018-03-01 20:42:36 -0500

    Check in

---
+++ src/main/java/com/example/ninadligade/imageeditor/utils/SpacesItemDecoration.java
@@ -0,0 +1,28 @@
+package com.example.ninadligade.imageeditor.utils;
+
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by ninadligade on 1/18/18.
+ */
+
+public class SpacesItemDecoration extends RecyclerView.ItemDecoration {
+    private int space;
+
+    public SpacesItemDecoration(int space) {
+        this.space = space;
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        if (parent.getChildAdapterPosition(view) == state.getItemCount() - 1) {
+            outRect.left = space;
+            outRect.right = 0;
+        }else{
+            outRect.right = space;
+            outRect.left = 0;
+        }
+    }
+}
